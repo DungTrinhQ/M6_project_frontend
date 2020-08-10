f8e83e9e2d42e660c79ee6310db45ab44ed6a364
Admin <thienvu453@gmail.com>
2020-08-10 17:02:56 +0700

    fix path from /accounts => /wall

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
       {path:'register', component:RegisterComponent},
       {path:'login',component:LoginComponent}
     ]},
-  {path: 'accounts',canActivate:[AuthGaurdService],component:MainwallComponent}
+  {path: 'wall',canActivate:[AuthGaurdService],component:MainwallComponent}
 ];
 
 @NgModule({
