8f59ee6719168b7079cc26b470bd9427997ca2b0
nguyendoan97 <nguyenhongdoan14@gmail.com>
2020-08-11 16:44:39 +0700

    sửa link css và trường avatarUrl vào service

--- src/app/modelbook/account-infomation/edit-info/edit-info.component.ts
+++ src/app/modelbook/account-infomation/edit-info/edit-info.component.ts
@@ -3,6 +3,8 @@ import {FormBuilder, FormGroup} from '@angular/forms';
 import {IAccount} from '../../../models/iaccount';
 import {AccountService} from '../../../service/account.service';
 import {ActivatedRoute, Router} from '@angular/router';
+import {AngularFireStorage} from '@angular/fire/storage';
+import {finalize} from 'rxjs/operators';
 
 @Component({
   selector: 'app-edit-info',
@@ -11,16 +13,27 @@ import {ActivatedRoute, Router} from '@angular/router';
 })
 export class EditInfoComponent implements OnInit {
   editUserProfile: FormGroup;
-  user: IAccount;
+  userAvatar:FormGroup;
+  user: IAccount = {
+    email:'',
+    password:'',
+    avatarUrl: ''
+  };
+  selectedImage:any = null;
+
   constructor(
     private fb: FormBuilder,
     private router: Router,
     private accountService: AccountService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private storage: AngularFireStorage
   ) { }
   id = +this.route.snapshot.paramMap.get('id');
 
   ngOnInit(): void {
+    this.userAvatar = this.fb.group({
+      avatar:[''],
+    });
     this.editUserProfile = this.fb.group({
       id: [''],
       email: [''],
@@ -29,9 +42,9 @@ export class EditInfoComponent implements OnInit {
       address: [''],
       phoneNumber: [''],
       dateOfBirth: [''],
+      avatarUrl:['']
     });
     this.findUserByID();
-    console.log(this.id)
   }
   findUserByID(){
     this.accountService.getAccount(this.id).subscribe((res : IAccount) =>{
@@ -43,8 +56,36 @@ export class EditInfoComponent implements OnInit {
     let data = this.editUserProfile.value;
     this.accountService.editAccountInfo(data, this.id).subscribe((res : IAccount) => {
       this.user = data;
+      console.log(this.user);
       // this.router.navigate([''])
     })
   }
 
+  updateAvatar() {
+    if(this.selectedImage !==null){
+      const filePath = `avatar/${this.selectedImage.name.split('.').slice(0,-1).join('.')}_${new Date().getTime()}`;
+      const fileRef = this.storage.ref(filePath);
+      this.storage.upload(filePath,this.selectedImage).snapshotChanges().pipe(
+        finalize(
+          ()=> fileRef.getDownloadURL().subscribe(url=>{
+            this.editUserProfile.value.avatarUrl = url;
+            alert("Ấn cập nhật để xác nhận thay đổi.")
+          })
+        )
+      ).subscribe();
+    }
+
+  }
+
+  loadImgFile(even:any) {
+    if(even.target.files && even.target.files[0]){
+      const reader = new FileReader();
+      reader.onload = (e:any) => this.user.avatarUrl = e.target.result;
+      reader.readAsDataURL(even.target.files[0]);
+      this.selectedImage = even.target.files[0];
+    }else {
+      this.selectedImage = null;
+    }
+
+  }
 }
