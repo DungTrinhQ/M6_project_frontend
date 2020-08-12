8430f1c9f227ee554a6722a39889929438c4de49
Admin <thienvu453@gmail.com>
2020-08-12 09:11:24 +0700

    add form basic main wall page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,9 @@ import {AngularFireStorageModule} from '@angular/fire/storage';
 import {AngularFireModule} from '@angular/fire';
 import {environment} from '../environments/environment';
 import {AngularFireDatabaseModule} from '@angular/fire/database';
+import { NavbarComponent } from './modelbook/layout/mainwall/navbar/navbar.component';
+import { InputSearchComponent } from './modelbook/layout/mainwall/navbar/input-search/input-search.component';
+import { ContentComponent } from './modelbook/layout/mainwall/content/content.component';
 
 @NgModule({
   declarations: [
@@ -21,6 +24,9 @@ import {AngularFireDatabaseModule} from '@angular/fire/database';
     RegisterComponent,
     LoginComponent,
     MainwallComponent,
+    NavbarComponent,
+    InputSearchComponent,
+    ContentComponent,
   ],
   imports: [
     BrowserModule,
